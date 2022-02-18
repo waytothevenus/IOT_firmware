eae1c8df7ed6152383b545b567a9f69219a1ce9f
waytothevenus <andyhung772@gmail.com>
2022-02-17 19:36:44 -0800

    py

--- bin/util/install-node.sh
+++ bin/util/install-node.sh
@@ -1,19 +1,21 @@
 # Install Node
 # sudo wget https://nodejs.org/download/release/v11.15.0/node-v11.15.0-linux-armv6l.tar.gz
-# sudo tar -xzf node-v11.15.0-linux-armv6l.tar.gz &&
-# cd /home/pi/node-v11.15.0-linux-armv6l/ &&
-# sudo cp -R * /usr/local/
+sudo wget https://unofficial-builds.nodejs.org/download/release/v16.14.0/node-v16.14.0-linux-armv6l.tar.gz
+
+sudo tar -xzf node-v16.14.0-linux-armv6l.tar.gz
+cd /home/pi/node-v16.14.0-linux-armv6l/
+sudo cp -R * /usr/local/
 
 # Install NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/latest/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/latest/install.sh | bash
 
-# Download yarn GPG key and add to trusted keys
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-# Add repo to list
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update
-# Install yarn
-sudo apt install -y yarn
+# # Download yarn GPG key and add to trusted keys
+# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# # Add repo to list
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+# sudo apt update
+# # Install yarn
+# sudo apt install -y yarn
 
 sudo node -v
 sudo yarn -v
