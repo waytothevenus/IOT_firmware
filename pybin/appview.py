5b026d6e4a13b2406b46267ab9e1bf390f0dea20
waytothevenus <andyhung772@gmail.com>
2022-02-25 21:43:38 -0800

    test

--- pybin/appview.py
+++ pybin/appview.py
@@ -1,7 +1,30 @@
+import os
 import webview
 
-webview.create_window(
-    'SmartCloud', url="/home/pi/firmware/static/test/index.html")
+"""
+An example of serverless app architecture
+"""
 
-DEBUG = True
-webview.start(debug=DEBUG)
+
+class Api():
+    def log(self, text):
+        print(text)
+
+    def removeItem(self, item):
+        print('Removed item %s' % item)
+
+    def editItem(self, item):
+        print('Edited item %s' % item)
+
+    def toggleItem(self, item):
+        print('Toggled item %s' % item)
+
+    def toggleFullscreen(self):
+        webview.windows[0].toggle_fullscreen()
+
+
+if __name__ == '__main__':
+    api = Api()
+    webview.create_window('Todos magnificos',
+                          'assets/index.html', js_api=api, min_size=(600, 450))
+    webview.start()
