fcb3489cd0dd3fdb01e269ec9d5f8993fc41db3c
waytothevenus <andyhung772@gmail.com>
2022-02-25 22:03:10 -0800

    test

--- pybin/app.py
+++ pybin/app.py
@@ -13,19 +13,6 @@ DEBUG = True
 
 
 class Api():
-    def __init__(self):
-        self.default_variable = False
-
-        self.HW_ID = self._get_hw_id()
-        if DEBUG:
-            print('Initialized: ' + self.HW_ID)
-
-    def init(self, params):
-        response = {
-            'message': 'Hello from Python {0}'.format(sys.version)
-        }
-        return json.dumps(response)
-
     def _get_hw_id(self):
         # Extract serial from cpuinfo file
         hw_id = "0000000000000000"
@@ -40,8 +27,29 @@ class Api():
 
         return hw_id
 
+    def __init__(self):
+        self.default_variable = False
+
+        self.HW_ID = self._get_hw_id()
+        if DEBUG:
+            print('Initialized: ' + self.HW_ID)
+
+    def init(self, params):
+        response = {
+            'message': 'Hello from Python {0}'.format(sys.version)
+        }
+        return json.dumps(response)
+
     def log(self, text):
         print('[Cloud] %s' % text)
+
+    def getRandomNumber(self, params):
+        randNum = random.randint(0, 100000000)
+        message = 'Random IO: {0}'.format(randNum)
+        response = {
+            'message': message
+        }
+        return json.dumps(response)
 
     def toggleFullscreen(self):
         webview.windows[0].toggle_fullscreen()
@@ -49,6 +57,21 @@ class Api():
 
 if __name__ == '__main__':
     api = Api()
-    webview.create_window('Smartcloud', '/home/pi/firmware/static/index.html',
-                          js_api=api, min_size=(600, 450))
+    # https: // pywebview.flowrl.com/guide/api.html  # webview-create-window
+    webview.create_window(
+        'Smartcloud',
+        url="/home/pi/firmware/static/index.html",
+        # url="https://lmorrow.ngrok.io/",
+        # url="",
+        js_api=api,
+        width=640,
+        height=350,
+        # frameless=True,
+        # on_top=False,
+        # fullscreen=False,
+        resizable=False,
+        text_select=False,
+        min_size=(320, 240),
+        background_color='#F00'
+    )
     webview.start(debug=DEBUG, http_server=True)
