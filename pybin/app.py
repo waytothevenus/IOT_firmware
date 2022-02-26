99bd270add24d4298317962e495c980044aecec2
waytothevenus <andyhung772@gmail.com>
2022-02-25 21:52:09 -0800

    test

---
+++ pybin/app.py
@@ -0,0 +1,57 @@
+import json
+import os
+import random
+import sys
+import time
+import webview
+
+"""
+An example of serverless app architecture
+"""
+
+DEBUG = True
+
+
+class Api():
+    def __init__(self):
+        self.default_variable = False
+
+        self.HW_ID = self._get_hw_id()
+        if DEBUG:
+            print('HWID: ' + self.HW_ID)
+
+    def init(self, params):
+        response = {
+            'message': 'Hello from Python {0}'.format(sys.version)
+        }
+        return json.dumps(response)
+
+    def _get_hw_id(self):
+        # Extract serial from cpuinfo file
+        hw_id = "0000000000000000"
+        try:
+            f = open('/proc/cpuinfo', 'r')
+            for line in f:
+                if line[0:6] == 'Serial':
+                    hw_id = line[10:26]
+            f.close()
+        except:
+            hw_id = "ERROR000000000"
+
+        return hw_id
+
+    def log(self, text):
+        print('[Cloud] %s' % text)
+
+    def toggleFullscreen(self):
+        webview.windows[0].toggle_fullscreen()
+
+
+if __name__ == '__main__':
+    api = Api()
+    webview.create_window('Smartcloud', 'assets/index.html',
+                          js_api=api, min_size=(600, 450))
+    webview.start()
+
+webview.create_window(
+    'SmartCloud', url="/home/pi/firmware/static/test/index.html", debug=DEBUG)
