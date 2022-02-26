19eb21514cfc327554c49b2a031f9c6b0a21a80b
waytothevenus <andyhung772@gmail.com>
2022-02-25 21:29:07 -0800

    test

--- pybin/appview.py
+++ pybin/appview.py
@@ -1,6 +1,7 @@
 import webview
 
-webview.create_window('SmartCloud', 'https://iot-cloud-lacymorrow.vercel.app/')
+webview.create_window(
+    'SmartCloud', url="/home/pi/firmware/static/test/index.html")
 
 DEBUG = True
 webview.start(debug=DEBUG)
