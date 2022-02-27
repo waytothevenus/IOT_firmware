ab8b2852522068681f79f2b968ca89fa2de6e34c
waytothevenus <andyhung772@gmail.com>
2022-02-27 13:14:40 -0800

    test

--- pybin/ip.py
+++ pybin/ip.py
@@ -18,15 +18,16 @@ class Api():
 
     def getWifiNetworks(self, params):
         networks = 'Error'
-        try:
-            ps = subprocess.Popen(
-                ('sudo', 'iwlist', 'wlan0', 'scan'), stdout=subprocess.PIPE)
-            process = subprocess.check_output(
-                ('grep', 'ESSID:'), stdin=ps.stdout)
-            ps.wait()
-            networks = process.decode("utf-8")
-        except:
-            return networks
+        ps = subprocess.Popen(
+            ('sudo', 'iwlist', 'wlan0', 'scan'), stdout=subprocess.PIPE)
+        print(ps)
+        process = subprocess.check_output(
+            ('grep', 'ESSID:'), stdin=ps.stdout)
+        print(process)
+        ps.wait()
+        networks = process.decode("utf-8")
+        print(networks)
+        return networks
 
 
 if __name__ == '__main__':
