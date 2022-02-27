e59676b530521dd8edfe154465cacc913f448657
waytothevenus <andyhung772@gmail.com>
2022-02-26 17:01:04 -0800

    test

--- pybin/ip.py
+++ pybin/ip.py
@@ -16,13 +16,9 @@ DEBUG = True
 class Api():
     # Usage: get`_ip_address('eth0') -> 192.160.0.110
     def getIpAddress(self):
-        process = subprocess.check_output(
-            "hostname --ip-address",
-            stderr=subprocess.STDOUT,
-            shell=True
-        )
+        process = subprocess.check_output(["hostname", "-I"]).split()[0]
         response = {
-            'message': process.decode("utf-8").strip('\n'),
+            'message': str(process),
         }
         if DEBUG:
             print('IP: ' + str(response))
