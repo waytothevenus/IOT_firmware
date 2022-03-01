80a537d6664dd003d3d76210c89149773bc48b58
waytothevenus <andyhung772@gmail.com>
2022-03-01 15:10:05 -0800

    test

--- bin/util/connect-wifi-network.sh
+++ bin/util/connect-wifi-network.sh
@@ -3,12 +3,12 @@
 network="$(wpa_cli -i wlan0 add_network)"
 # > 0 # this is the new network's ID
 echo "Setting network $network to $1"
-wpa_cli set_network '$network' ssid '"'$1'"'
+wpa_cli set_network "$network" ssid '"'$1'"'
 # wpa_cli set_network '$network' psk '""'
 if [[ ! -z $2 ]]; then
 	echo "Setting network $network password to $2"
-	wpa_cli set_network '$network' psk '"'$2'"'
+	wpa_cli set_network "$network" psk '"'$2'"'
 fi
-wpa_cli enable_network '$network'
+wpa_cli enable_network "$network"
 wpa_cli save_config
-wpa_cli -i wlan0 reassociate '$network'
+wpa_cli -i wlan0 reassociate "$network"
