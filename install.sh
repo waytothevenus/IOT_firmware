cba5db637ba43e66c763f3ff4068ee404b569162
waytothevenus <andyhung772@gmail.com>
2022-11-08 03:10:15 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,44 @@
 #!/bin/sh
 
+echo "> Smartcloud Setup ---"
+
+
+
 # sudo systemctl enable getty@tty1.service
 # sudo apt-get update -y
 
 # Install Git
+echo "> Install Git"
 if ! command -v git &> /dev/null
 then
 	sudo apt-get install -y git
 fi
 
-# Set git config (rebase strategy)
 
 # Clone repo
+echo "> Delete old repo"
 sudo rm -rf /home/pi/firmware
 # TODO use main branch (CURRENTLY USING DEV)
+echo "> Clone firmware"
 sudo git clone --single-branch --branch main https://github.com/lacymorrow/iot-firmware.git /home/pi/firmware
 
+# Set git config (rebase strategy)
+echo "> Set pull behavior"
 sudo git -C /home/pi/firmware/ config pull.rebase false
+
+echo "> Make bin folder executable"
 sudo chmod -R 755 /home/pi/firmware/bin
 
 # Backup Config files
+echo "> Backup config files"
 sudo cp /boot/config.txt /boot/config.old.txt
 
 # Enable SSH
+echo "> Enable SSH"
 sudo touch /boot/ssh
 
 # Boot files
+echo "> Copy boot files"
 sudo rm /etc/rc.local
 sudo cp /home/pi/firmware/root/etc/rc.local /etc/rc.local
 
@@ -45,17 +58,22 @@ sudo cp -R /home/pi/firmware/root/etc/default /etc/default
 
 # TODO Change password
 # Set user password
+echo "> Set user password pi:pi"
 sudo echo "pi:pi" > pass.txt
 sudo chpasswd < pass.txt
+sudo rm pass.txt
 
 # raspi-config
+echo "> Running Raspi Config"
 sudo bash /home/pi/firmware/bin/setup/raspi-config-setup.sh
 
 # Delete Raspi-config wizard
+echo "> Delete Raspi setup wizard"
 sudo cp /etc/xdg/autostart/piwiz.desktop /home/pi/firmware
 sudo rm /etc/xdg/autostart/piwiz.desktop
 
 # TODO REMOVE: Setup home wifi network; replace <ssid> and <password>
+# echo "> Setup wifi network"
 # sudo bash /home/pi/firmware/bin/util/connect-wifi-network.sh <ssid> <password>
 # sudo bash /home/pi/firmware/bin/util/connect-wifi-network.sh Castle homesweethome
 
@@ -66,10 +84,12 @@ if [ ! -e /home/pi/.first_run ]; then
 fi
 
 # Install Temp/Humidity Drivers
+echo "> Install temp/humidity"
 bash /home/pi/firmware/bin/setup/install-temperhum.sh
 
 # Install display drivers
 # This forces a reboot
+echo "> Install display"
 sudo bash /home/pi/firmware/bin/setup/install-display.sh
 
 # Reboot (if display script didn't already)
