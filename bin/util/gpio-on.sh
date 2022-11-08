bb89eac5632450e31e529d754dc845f1f75fccb1
waytothevenus <andyhung772@gmail.com>
2022-11-08 02:25:32 -0500

    gpio

--- bin/util/gpio-on.sh
+++ bin/util/gpio-on.sh
@@ -1,11 +1,23 @@
-#   Exports pin to userspace
-echo "18" > /sys/class/gpio/export                  
+if [[ ! -z $1 ]]; then
+	echo "Set GPIO $1 to ON!"
+    
+    # Exports pin to userspace
+    echo $1 > /sys/class/gpio/export                  
 
-# Sets pin 18 as an output
-echo "out" > /sys/class/gpio/gpio18/direction
+    # Sets pin as an output
+    echo "out" > "/sys/class/gpio/gpio$1/direction"
 
-# Sets pin 18 to high
-echo "1" > /sys/class/gpio/gpio18/value
+    # Sets pin to high
+    echo "1" > "/sys/class/gpio/gpio$1/value"
+else
+	echo "Set GPIO 26 to ON!"
+    
+    # Exports pin to userspace
+    echo "26" > /sys/class/gpio/export                  
 
-# Sets pin 18 to low
-# echo "0" > /sys/class/gpio/gpio18/value 
+    # Sets pin as an output
+    echo "out" > "/sys/class/gpio/gpio26/direction"
+
+    # Sets pin to high
+    echo "1" > "/sys/class/gpio/gpio26/value"
+fi
